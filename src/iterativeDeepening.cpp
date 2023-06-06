90199e65bf2f3e496793e052d835e3dbddfa300d
Alexander Mehls <alexmehls@t-online.de>
2023-06-06 14:43:30 +0200

    Cleaned up minimax and iterative deepening

--- src/iterativeDeepening.cpp
+++ src/iterativeDeepening.cpp
@@ -7,53 +7,60 @@
 namespace IterativeDeepening
 {
 
-    void getMoveIterativeDeepening(uint8_t &x, uint8_t &y, uint8_t **board, uint8_t playerNumber, uint32_t maxTime, const std::chrono::time_point<std::chrono::steady_clock> startTime, const std::function<int(uint8_t **)> &heuristic){
+    void getMoveIterativeDeepening(uint8_t &x, uint8_t &y, uint8_t **board, uint8_t playerNumber, const std::function<int(uint8_t **, uint8_t)> &heuristic, const std::chrono::time_point<std::chrono::steady_clock> &stopTime){
 
         uint8_t iterationDepth = 1;
         uint8_t tempx;
         uint8_t tempy;
-        bool islastMove = false;
+        bool miniMaxReachedMaxDepth = true;
 
-        while(timeForNextIteration(iterationDepth, startTime, maxTime)){
+        std::chrono::duration<double> lastIterationDuration(0);
+
+        while(timeForNextIteration(iterationDepth, stopTime, lastIterationDuration) && miniMaxReachedMaxDepth){
             x = tempx;
             y = tempy;
 
+            auto iterationStart = std::chrono::steady_clock::now();
             // If Minimax returns true --> continueCalculation
-            if(Minimax::getMoveMinimaxTimeCheck(tempx, tempy, board, playerNumber, iterationDepth, maxTime, startTime, islastMove, heuristic)){
+            if(Minimax::getMoveMinimax(tempx, tempy, miniMaxReachedMaxDepth, board, playerNumber, iterationDepth, heuristic, stopTime)){
                 std::cout << "Found x,y: " << int(tempx) << int(tempy) << " in depth " << int(iterationDepth) << std::endl;
                 x = tempx;
                 y = tempy;
             }
             else{
-                std::cout << "\nMinimax couldn't finish in time or no new nodes available\n" << std::endl;
+                std::cout << "\nMinimax couldn't finish in time\n" << std::endl;
+                /*
                 //if set, this is the last move
                 if(islastMove){
                     std::cout << "\nIt's the last move\n" << std::endl;
                     x = tempx;
                     y = tempy;
                 }
+                */
                 return;
             }
+            lastIterationDuration = std::chrono::steady_clock::now() - iterationStart;
 
             iterationDepth++;
         }
         return;
     }
 
-    bool timeForNextIteration(uint8_t iterationDepth, const std::chrono::time_point<std::chrono::steady_clock> startTime, uint32_t maxTime) {
+    bool timeForNextIteration(uint8_t iterationDepth, const std::chrono::time_point<std::chrono::steady_clock> &stopTime, const std::chrono::duration<double> &lastIterationDuration) {
 
         //calculate left time
         auto currentTime = std::chrono::steady_clock::now();
-        int64_t spentTime = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - startTime).count();
-        int64_t timeLeft = maxTime - spentTime;
+        int64_t timeLeft = std::chrono::duration_cast<std::chrono::milliseconds>(stopTime - currentTime).count();
         std::cout << "\nTime left before " << int(iterationDepth) << " depth is\t" << timeLeft << std::endl;
 
         //calculate estimated Time by getting average branching faktor of previous iterations
         if (iterationDepth != 1) {
-            double basis = std::pow(spentTime / 0.07, (1.0 / (iterationDepth - 1)));
+            int64_t t_previous = std::chrono::duration_cast<std::chrono::nanoseconds>(lastIterationDuration).count();
+            std::cout << "Previous duration: " << t_previous << "ns (" << (double)t_previous / 1000000 << "ms)" << std::endl;
+            double basis = std::pow(t_previous, (1.0 / (iterationDepth - 1)));
             std::cout << "Branching Faktor so far: \t" << basis << std::endl;
             double estimatedTime = std::pow(basis, iterationDepth);
-            estimatedTime = estimatedTime * 0.07;
+            estimatedTime = estimatedTime / 1000000;
             std::cout << "Estimated time TEST: \t" << estimatedTime << std::endl;
 
             // If enough time left
@@ -71,19 +78,5 @@ namespace IterativeDeepening
         }
     }
 
-    bool checkForTimeEnd(uint32_t maxTime, const std::chrono::time_point<std::chrono::steady_clock> startTime) {
-
-        auto currentTime = std::chrono::steady_clock::now();
-        int64_t spentTime = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - startTime).count();
-        int64_t timeLeft = maxTime - spentTime;
-
-        // If enough time left
-        if(timeLeft > 5){
-            return 1;
-        }
-        else{
-            return 0;
-        }
-    }
 } //namespace IterativeDeepening
 
--- src/minimax.cpp
+++ src/minimax.cpp
@@ -31,6 +31,13 @@ namespace Minimax
     }
 
     void getMoveMinimax(uint8_t &x, uint8_t &y, uint8_t **board, uint8_t playerNumber, uint8_t maxDepth, const std::function<int(uint8_t **, uint8_t)> &heuristic) {
+        bool reached;
+        auto stopTime = std::chrono::steady_clock::now() + std::chrono::hours(24);
+        getMoveMinimax(x, y, reached, board, playerNumber, maxDepth, heuristic, stopTime);
+        return;
+    }
+
+    bool getMoveMinimax(uint8_t &x, uint8_t &y, bool &reachedMaxDepth, uint8_t **board, uint8_t playerNumber, uint8_t maxDepth, const std::function<int(uint8_t **, uint8_t)> &heuristic, const std::chrono::time_point<std::chrono::steady_clock> &stopTime) {
 
         std::stack<Node> nodeStack;
         nodeStack.push(std::move(Node()));
@@ -45,6 +52,7 @@ namespace Minimax
         Move currentRootMove = root.validMoves.front();
         x = currentRootMove.x;
         y = currentRootMove.y;
+        reachedMaxDepth = false;
 
         /*
         std::cout << "Moves (root):" << std::endl;
@@ -55,11 +63,17 @@ namespace Minimax
 
         // Iterative DFS
         while (!nodeStack.empty()) {
+
+            if(!checkTimeLeft(stopTime)) {
+                std::cout << "No Time left, so break calculation and return with previous iteration" << std::endl;
+                return false;
+            }
+
             Node &currentNode = nodeStack.top();
             //std::cout << "Current node player " << (int)currentNode.player << " on depth " << (int)depth << std::endl;
 
             // All moves done
-            if (currentNode.hasValidMoves()) {
+            if (!currentNode.hasValidMoves()) {
                 //std::cout << "Node finished for player " << (int)currentNode.player << " on depth " << (int)depth << std::endl;
                 if (depth == 0) {
                     break;
@@ -81,7 +95,7 @@ namespace Minimax
                         }
                     }
                 }
-                    // Minimize
+                // Minimize
                 else {
                     if (compareVal < node.value) {
                         node.value = compareVal;
@@ -111,12 +125,21 @@ namespace Minimax
                 isLeaf = true;
                 newNode.value = Heuristics::evaluateEndState(newNode.board, playerNumber);
                 //std::cout << "Leaf node (game ended) after player " << (int)currentNode.player << " on depth " << (int)depth + 1 << std::endl;
+
+                /*
+                // If game ended in depth 0 (needed in iterativeDeepening)
+                if(depth == 0){
+                    islastMove = true;
+                    std::cout << "Depth: " << int(depth) << std::endl;
+                }
+                */
             }
-                // If reached max depth
+            // If reached max depth
             else if (depth + 1 >= maxDepth) {
                 isLeaf = true;
                 newNode.value = heuristic(newNode.board, playerNumber);
                 //std::cout << "Leaf node (max depth reached) after player " << (int)currentNode.player << " on depth " << (int)depth + 1 << std::endl;
+                reachedMaxDepth = true;
             }
 
             // Leaf nodes are handled immediately -> aren't added to the stack
@@ -131,13 +154,13 @@ namespace Minimax
                         }
                     }
                 }
-                    // Minimize
+                // Minimize
                 else {
                     if (newNode.value < currentNode.value) {
                         currentNode.value = newNode.value;
                     }
                 }
-            }
+            } 
             else {
                 if (newNode.player == playerNumber) {
                     newNode.value = INT_MIN;
@@ -157,179 +180,20 @@ namespace Minimax
                 nodeStack.push(std::move(newNode));
             }
         }
-        return;
+        return true;
     }
 
+    bool checkTimeLeft(const std::chrono::time_point<std::chrono::steady_clock> &stopTime) {
+        auto currentTime = std::chrono::steady_clock::now();
+        int64_t timeLeft = std::chrono::duration_cast<std::chrono::milliseconds>(stopTime - currentTime).count();
 
-    bool getMoveMinimaxTimeCheck(uint8_t &x, uint8_t &y, uint8_t **board, uint8_t playerNumber, uint8_t maxDepth, uint32_t maxTime, const std::chrono::time_point<std::chrono::steady_clock> startTime, bool &islastMove, const std::function<int(uint8_t **)> &heuristic) {
-
-        std::stack<Node> nodeStack;
-        nodeStack.push(std::move(Node()));
-
-        Node &root = nodeStack.top();
-        root.board = copy2DArr(board, GameDetails::boardHeight, GameDetails::boardWidth);
-        Moves::populateValidMoves(root.validMoves, board, playerNumber);
-        root.value = INT_MIN;
-        root.player = playerNumber;
-
-        uint8_t depth = 0;
-        Move currentRootMove = root.validMoves.back();
-        x = currentRootMove.x;
-        y = currentRootMove.y;
-
-        bool continue_calculation = false;
-
-        /*
-        std::cout << "Moves (root):" << std::endl;
-        for (auto &element : nodeStack.top().validMoves) {
-            std::cout << "X = " << (int)element.x << ", Y = " << (int)element.y << std::endl;
+        // If enough time left
+        if(timeLeft > 5){
+            return 1;
         }
-        */
-
-        // Iterative DFS
-        while (!nodeStack.empty()) {
-            if(IterativeDeepening::checkForTimeEnd(maxTime, startTime)) {
-                Node &currentNode = nodeStack.top();
-                //std::cout << "Current node player " << (int)currentNode.player << " on depth " << (int)depth << std::endl;
-
-                // All moves done
-                if (currentNode.validMoves.empty()) {
-                    //std::cout << "Node finished for player " << (int)currentNode.player << " on depth " << (int)depth << std::endl;
-                    if (depth == 0) {
-                        break;
-                    }
-
-                    int compareVal = currentNode.value;
-
-                    --depth;
-                    nodeStack.pop();
-
-                    Node &node = nodeStack.top();
-                    // Maximize
-                    if (node.player == playerNumber) {
-                        if (compareVal > node.value) {
-                            node.value = compareVal;
-                            if (depth == 0) {
-                                x = currentRootMove.x;
-                                y = currentRootMove.y;
-                            }
-                        }
-                    }
-                        // Minimize
-                    else {
-                        if (compareVal < node.value) {
-                            node.value = compareVal;
-                        }
-                    }
-
-                    continue;
-                }
-
-                // Generate new node
-                Move currentMove = currentNode.validMoves.back();
-                if (depth == 0) {
-                    currentRootMove = currentMove;
-                }
-                currentNode.validMoves.pop_back();
-
-                Node newNode;
-                // new Board
-                newNode.board = copy2DArr(currentNode.board, GameDetails::boardHeight, GameDetails::boardWidth);
-                Moves::makeMove(newNode.board, currentMove.x, currentMove.y, currentNode.player);
-
-                // new Player + Moves
-                newNode.player = nextValidPlayerMoves(newNode.validMoves, newNode.board, currentNode.player);
-
-                bool isLeaf = false;
-                // If game ended
-                if (newNode.player == 0) {
-                    isLeaf = true;
-
-                    // If game ended in depth 0 (needed in iterativeDeepening)
-                    if(depth == 0){
-                        islastMove = true;
-                        std::cout << "Depth: " << int(depth) << std::endl;
-                    }
-
-                    // Win
-                    if (highestPieceCount(newNode.board) == playerNumber) {
-                        newNode.value = INT_MAX;
-                    }
-                        // Loss
-                    else {
-                        newNode.value = INT_MIN;
-                    }
-                    //std::cout << "Leaf node (game ended) after player " << (int)currentNode.player << " on depth " << (int)depth + 1 << std::endl;
-                }
-                    // If reached max depth
-                else if (depth + 1 >= maxDepth) {
-                    isLeaf = true;
-                    newNode.value = heuristic(newNode.board);
-                    //std::cout << "Leaf node (max depth reached) after player " << (int)currentNode.player << " on depth " << (int)depth + 1 << std::endl;
-                    continue_calculation = true;
-                }
-
-                // Leaf nodes are handled immediately -> aren't added to the stack
-                if (isLeaf) {
-                    // Maximize
-                    if (currentNode.player == playerNumber) {
-                        if (newNode.value > currentNode.value) {
-                            currentNode.value = newNode.value;
-                            if (depth == 0) {
-                                x = currentRootMove.x;
-                                y = currentRootMove.y;
-                            }
-                        }
-                    }
-                        // Minimize
-                    else {
-                        if (newNode.value < currentNode.value) {
-                            currentNode.value = newNode.value;
-                        }
-                    }
-                } else {
-                    if (newNode.player == playerNumber) {
-                        newNode.value = INT_MIN;
-                    } else {
-                        newNode.value = INT_MAX;
-                    }
-
-                    ++depth;
-                    /*
-                    std::cout << "Created player " << (int)newNode.player << " node on depth " << (int)depth << std::endl;
-                    std::cout << "Moves:" << std::endl;
-                    for (auto &element : newNode.validMoves) {
-                        std::cout << "X = " << (int)element.x << ", Y = " << (int)element.y << std::endl;
-                    }
-                    */
-                    nodeStack.push(std::move(newNode));
-                }
-            }
-            else{
-                std::cout << "No Time left, so break calculation and return with previous iteration" << std::endl;
-                continue_calculation = false;
-                return continue_calculation;
-            }
+        else{
+            return 0;
         }
-
-        return continue_calculation;
-    }
-
-    uint8_t highestPieceCount(uint8_t **board) {
-        std::vector<int> pieceCounts(GameDetails::playerCount, 0);
-        uint8_t boardVal;
-
-        for (uint8_t i = 0; i < GameDetails::boardHeight; ++i) {
-            for (uint8_t j = 0; j < GameDetails::boardWidth; ++j) {
-                boardVal = board[i][j];
-                if (boardVal >= 1 && boardVal <= GameDetails::playerCount) {
-                    ++pieceCounts[boardVal - 1];
-                }
-            }
-        }
-
-        auto maxCountIt = std::max_element(pieceCounts.begin(), pieceCounts.end());
-        return std::distance(pieceCounts.begin(), maxCountIt) + 1;
     }
 
     uint8_t nextValidPlayerMoves(std::vector<Move> &validMoves, uint8_t **board, uint8_t currentPlayer) {
