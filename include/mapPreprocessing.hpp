be292e7a9d3d57b6c8bd6117cbf1bc91230a5909
Alexander Mehls <alexmehls@t-online.de>
2023-06-07 18:36:09 +0200

    tweaks to mask creation

--- include/mapPreprocessing.hpp
+++ include/mapPreprocessing.hpp
@@ -2,7 +2,7 @@
 
 namespace MapPreprocessing
 {
-    const uint8_t ValueEdge = 2;
+    //const uint8_t ValueEdge = 2;
 
     uint8_t** createValueMask(uint8_t** board);
     int evaluateEdges(uint8_t** board, uint8_t** mask, uint8_t playerNumber);
--- src/mapPreprocessing.cpp
+++ src/mapPreprocessing.cpp
@@ -7,30 +7,38 @@ namespace MapPreprocessing
 {
     uint8_t** createValueMask(uint8_t** board) {
         // Create mask with the same dimensions as the board
-        uint8_t** mask = create2DArr<uint8_t>(GameDetails::boardHeight, GameDetails::boardWidth);
+        uint8_t** mask = createZeroInit2DArr<uint8_t>(GameDetails::boardHeight, GameDetails::boardWidth);
 
         // Iterate over the board elements
         for (int i = 0; i < GameDetails::boardHeight; i++) {
             for (int j = 0; j < GameDetails::boardWidth; j++) {
                 // for all fields that are a valid place for a piece
-                if (board[i][j] != 13) {
-                    int NumberOfBoarderingEdges = 0;
-                    //check all four directions to determin if it is an edge
-                    //down
-                    if (i == GameDetails::boardHeight - 1 || board[i+1][j] == 13)
-                        NumberOfBoarderingEdges++;
-                    //up
-                    if (i == 0 || board[i-1][j] == 13)
-                        NumberOfBoarderingEdges++;
-                    //right
-                    if (j == GameDetails::boardWidth - 1 || board[i][j+1] == 13)
-                        NumberOfBoarderingEdges++;
-                    //left
-                    if (j == 0 || board[i][j-1] == 13)
-                        NumberOfBoarderingEdges++;
-                    
-                    mask[i][j] = NumberOfBoarderingEdges * ValueEdge; //+1; for also counting pieces
+                if (board[i][j] == 13) {
+                    continue;
                 }
+                uint8_t tileValue = 1;
+
+                
+
+                /*
+                int NumberOfBoarderingEdges = 0;
+                //check all four directions to determin if it is an edge
+                //down
+                if (i == GameDetails::boardHeight - 1 || board[i+1][j] == 13)
+                    NumberOfBoarderingEdges++;
+                //up
+                if (i == 0 || board[i-1][j] == 13)
+                    NumberOfBoarderingEdges++;
+                //right
+                if (j == GameDetails::boardWidth - 1 || board[i][j+1] == 13)
+                    NumberOfBoarderingEdges++;
+                //left
+                if (j == 0 || board[i][j-1] == 13)
+                    NumberOfBoarderingEdges++;
+                
+                mask[i][j] = NumberOfBoarderingEdges * ValueEdge; //+1; for also counting pieces
+                */
+                mask[i][j] = tileValue;
             }
         }
         return mask;
